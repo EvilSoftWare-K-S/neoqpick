24e72e9852420d3a1841d3b8d7bca26dbe8387a5
Sergey Krahmalyov <sergey.krahmaliov@gmail.com>
2026-09-17 01:39:24 +0400

    init: necessary settings, basic styles, basic architecture

---
+++ widgets/footer/footer.tsx
@@ -0,0 +1,4 @@
+import footer from "./footer.module.css";
+export function Footer(): React.JSX.Element {
+  return <footer className={footer.footer}>f</footer>;
+}
---
+++ widgets/header/header.tsx
@@ -0,0 +1,4 @@
+import header from "./header.module.css";
+export function Header(): React.JSX.Element {
+  return <header className={header.header}>h</header>;
+}
